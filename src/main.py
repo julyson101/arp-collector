64d41cde2574b92295c02b788fc1d63b553af03c
Julyson Tayde <julyson@petronet.local>
2026-05-06 15:34:39 +0000

    Initial commit: ARP collector project

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 import yaml
 from arp_collector import collect_arp
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ROOT_DIR = BASE_DIR.parent
 
 
-INVENTORY_FILE = "inventory/devices.yml"
+INVENTORY_FILE = ROOT_DIR / "inventory" / "devices.yml"
 OUTPUT_FILE = "output/arp_output.txt"
 
 
